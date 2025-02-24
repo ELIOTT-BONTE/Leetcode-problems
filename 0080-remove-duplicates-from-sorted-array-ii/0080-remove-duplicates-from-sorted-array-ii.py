91e230e7e0e8ee5549042c89a08bdd5df67b7932
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-02-24 10:33:33 +0100

    Time: 70 ms (26.11%), Space: 15.4 MB (6.2%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -7,10 +7,10 @@ class Solution(object):
         :rtype: int
         """
 
-        i = 0
+        i = 0 #points to the last space where we can insert values
 
         for num in nums:
-            if i < 2 or num != nums[i-2]:
+            if i < 2 or num != nums[i-2]: # chec if we can insert the current iterated number
                 nums[i] = num
                 i += 1
         return i
