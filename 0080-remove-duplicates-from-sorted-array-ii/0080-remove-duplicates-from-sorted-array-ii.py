de773fea75458723ae898bb1890b72420990bc34
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-02-24 10:46:17 +0100

    Time: 69 ms (28.15%), Space: 15.4 MB (22.39%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,16 +1,16 @@
-from collections import defaultdict
-
 class Solution(object):
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-
-        i = 0 #points to the last space where we can insert values
+        i = 0
 
         for num in nums:
-            if i < 2 or num != nums[i-2]: # chec if we can insert the current iterated number
+            if i < 2 or num != nums[i-2]:
+                # then insert it
                 nums[i] = num
+                # and increment pointer
                 i += 1
+        
         return i
