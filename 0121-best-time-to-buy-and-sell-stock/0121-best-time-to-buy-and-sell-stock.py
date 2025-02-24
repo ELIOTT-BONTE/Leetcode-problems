1866e339a3e1a19c3a61383b8e15230e9977703e
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-02-24 15:16:15 +0100

    Time: 130 ms (26.21%), Space: 19 MB (90.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,15 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        minPrice = float("inf")
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        maxDiff = 0
+        mini = float("inf")
+        maxi = 0
 
         for p in prices:
-            minPrice = min(minPrice, p)
-            maxProfit = max(maxProfit, p-minPrice)
-        return maxProfit
+            mini = min(mini, p)
+            maxDiff = max(maxDiff, p - mini)
+
+        return maxDiff
