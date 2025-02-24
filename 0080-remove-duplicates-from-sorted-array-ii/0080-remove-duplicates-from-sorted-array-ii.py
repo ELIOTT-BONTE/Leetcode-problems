00e8fb7008d9075dac2fc6f84ae1206bf32e92e9
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-02-24 10:32:30 +0100

    Time: 73 ms (19.81%), Space: 15.3 MB (35.32%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        i = 0
+
+        for num in nums:
+            if i < 2 or num != nums[i-2]:
+                nums[i] = num
+                i += 1
+        return i
