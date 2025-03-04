756899fa1d4d250efa2647bae3da97fc699d9415
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-04 10:31:13 +0100

    Time: 4 ms (21.71%), Space: 19.5 MB (98.85%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,29 +1,20 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-
-        result = []
         i = 0
         n = len(intervals)
-
-        # find merge point
-
+        result = []
 
         while i < n and intervals[i][1] < newInterval[0]:
             result.append(intervals[i])
             i += 1
-
-        # merge newInterval what it overlaps (could be many intervals)
         while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[i][0])
-            newInterval[1] = max(newInterval[1], intervals[i][1])
+            newInterval[0], newInterval[1] = min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])
+            
+            i += 1
+        result.append(newInterval)
+        while i < n:
+            if intervals[i][0] > newInterval[1]:
+                result.append(intervals[i])
             i += 1
 
-        result.append(newInterval)
-
-        #merge what is after
-        while i < n:
-            result.append(intervals[i])
-            i+=1
-
         return result
-
