40f0cf07e8b79a6020315367844d0d2de0bc88f9
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-17 22:47:00 +0100

    Time: 3 ms (24.83%), Space: 12.7 MB (20.1%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,20 +1,28 @@
-class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        directions = [(1,0),(0,-1),(-1,0),(0,1)]
-        original_color = image[sr][sc] 
-
-
-        if color == original_color:
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+        if image[sr][sc] == color:
             return image
         
-        def colorize(r, c):
-            if r < 0 or r > len(image)-1 or c < 0 or c > len(image[0])-1 or image[r][c] == color:
-                pass
-            elif image[r][c] == original_color :
-                image[r][c] = color
-                for d in directions:
-                    colorize(r+d[0], c+d[1])
+        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 
-        colorize(sr, sc)
+        def fill(x,y, original):
+            if image[x][y] == color:
+                return
+            else:
+                image[x][y] = color
+                for dx, dy in directions:
+                    if 0 <= dx + x < len(image) and 0 <= dy + y < len(image[0]) and image[dx + x][dy + y] == original:
+                        fill(dx + x, dy + y, original)
+
+        original_color = image[sr][sc]
+            
+        fill(sr, sc, original_color)
 
         return image
