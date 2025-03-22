d265aa273403a2d8224c32297ec36d7bb7fc2681
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-22 23:49:11 +0100

    Time: 0 ms (100%), Space: 17.7 MB (58%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.py
+++ 0874-backspace-string-compare/0874-backspace-string-compare.py
@@ -1,20 +1,13 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        #store strings into stacks
-        stackS=[]
-        stackT=[]
-        #when encountering special character, pop last elem
-        for i in s:
-            if i == "#":
-                if stackS:
-                    stackS.pop()
-            else:
-                stackS.append(i)
-        for i in t:
-            if i == "#":
-                if stackT:
-                    stackT.pop()
-            else:
-                stackT.append(i)
-        #compare stacks
-        return stackS == stackT
+        def helper(s):
+            skip = 0 #keep track of how many characters when need to skip
+            for x in reversed(s):
+                if x == "#":
+                    skip += 1
+                elif skip:
+                    skip -= 1
+                else:
+                    yield x #makes this function a generator
+        
+        return all(x == y for x,y in itertools.zip_longest(helper(s),helper(t))) #compare characters being generated
