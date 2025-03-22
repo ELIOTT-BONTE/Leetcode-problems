3e7add52a5ac343c88c3af07f90692c4662f8bc8
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-22 10:15:59 +0100

    Time: 5 ms (17.96%), Space: 20.2 MB (8.18%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,9 +1,16 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        encountered = set()
-        for i in intervals:
-            for y in range(i[0],i[1]):
-                if y in encountered:
-                    return False
-                encountered.add(y)
+        # sort intervals depending on their first element
+        intervals.sort(key=lambda interval : interval[0])
+
+        print(intervals)
+
+        # check if second element of i is more than i+1
+        # if yes, return False
+        for i in range(len(intervals)-1):
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
+
+        # after iterating through all intervals, return True
+
         return True
