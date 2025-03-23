2a95a83f82b17060b653a4f663c84fed57c63959
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-23 14:22:29 +0100

    Time: 16 ms (45.16%), Space: 17.9 MB (47.02%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -11,7 +11,9 @@ class Solution(object):
             if i == 0:
                 res[i] = 0
             else :
-                res[i] = res[i // 2] + (i % 2)
+                res[i] = res[i // 2] + (i % 2) # number of bits is derived from number of bit of half
+                # indeed, left shift of 4 = 8
+                # left shift of 4 + 1bit = 9...
         for i in range(0,n+1):
             dp(i)
 
