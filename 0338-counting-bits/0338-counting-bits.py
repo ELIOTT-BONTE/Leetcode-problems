720e8829000b6907200cae8ccb783511998fb1d8
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-23 18:07:19 +0100

    Time: 7 ms (81.25%), Space: 17.9 MB (47.02%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,18 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-
         res = [0] * (n+1)
 
-        def dp(i):
+        i = 0
+        for i in range(0, n+1):
             if i == 0:
                 res[i] = 0
-            else :
-                res[i] = res[i // 2] + (i % 2) # number of bits is derived from number of bit of half
-                # indeed, left shift of 4 = 8
-                # left shift of 4 + 1bit = 9...
-        for i in range(0,n+1):
-            dp(i)
+            else:
+                res[i] = res[i//2] + (i%2)
 
         return res
-        
