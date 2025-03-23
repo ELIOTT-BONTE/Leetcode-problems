47867adb19076bf9cef803478d8c240f7e50bd58
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-23 14:20:38 +0100

    Time: 10 ms (56.91%), Space: 18 MB (13.58%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,4 +1,19 @@
-class Solution:
-    def countBits(self, n: int) -> List[int]:
-        #shorter brutefore
-        return [bin(i).count("1") for i in range(n+1)]
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+
+        res = [0] * (n+1)
+
+        def dp(i):
+            if i == 0:
+                res[i] = 0
+            else :
+                res[i] = res[i // 2] + (i % 2)
+        for i in range(0,n+1):
+            dp(i)
+
+        return res
+        
