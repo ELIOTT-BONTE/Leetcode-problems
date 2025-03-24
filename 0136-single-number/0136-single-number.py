621b59065708b1b26c5685c733b030f3c69e4177
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-24 11:56:48 +0100

    Time: 8 ms (39.44%), Space: 14.6 MB (17.42%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 class Solution(object):
     def singleNumber(self, nums):
         """
@@ -7,7 +5,6 @@ class Solution(object):
         :rtype: int
         """
         encountered = set()
-        res = []
         for n in nums:
             if n in encountered:
                 encountered.remove(n)
