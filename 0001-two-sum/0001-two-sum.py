bac4ae9eb67377b0d357bb3e41f0326ebe631301
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-25 20:44:33 +0100

    Time: 0 ms (100%), Space: 13.1 MB (77.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,26 @@
 from collections import defaultdict
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #hashmap {key:value}
+        #iterate through array
+        #check difference target - val
+        #if difference in hashmap, then
+        #return both
+        #otherwise, add iterated val and index
+
         encountered = defaultdict(int)
-        for i,v in enumerate(nums):
-            diff = target - v
-            if diff in encountered :
-                return i, encountered[diff]
-            encountered[v] = i
+        i = 0
+        while i <= len(nums)-1:
+            diff = target - nums[i]
+            if diff in encountered:
+                return encountered[diff],i
+            else:
+                encountered[nums[i]] = i
+            i += 1
+            
