423115677d44f37a1b319c057fdd47b1bfdf35eb
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-27 07:52:18 +0100

    Time: 51 ms (93.92%), Space: 17.4 MB (81.96%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,11 +1,11 @@
-# bruteforce
+from math import sqrt
 
-import math
-import numpy as np
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
 
-class Solution:
-    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # order points with lambda function x: x[0]**2+x[1]**2
-        # return k first elements
-        points.sort(key = lambda x: x[0]**2+x[1]**2)
-        return points[:k]
+        return sorted(points, key=lambda p: sqrt((0-p[0])**2 + (0-p[1])**2))[:k]
