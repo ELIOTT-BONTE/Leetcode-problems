c44e84dff7b9cf1b4b371925237fc9f06d382e40
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-03-29 17:10:24 +0100

    Time: 671 ms (91.13%), Space: 18 MB (97.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,15 +1,13 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # intialize dp array
-        # base case 0 coins yield 0 sum
-        # induction case n sum is yielded by [(n-val) +1] coins where val is any of our coins
+        # initalize dp array
+        # for each n, min( current value, dp[n - coin]+1)
 
-        dp = [amount+1]*(amount+1)
+        dp = [amount+1] * (amount+1)
         dp[0] = 0
 
-        for i in range(1,amount+1):
+        for i in range(amount+1):
             for coin in coins:
                 if i >= coin:
-                    dp[i] = min(dp[i], dp[i - coin]+1)
-
-        return dp[amount] if dp[amount] <= amount else -1 
+                    dp[i] =min(dp[i], dp[i-coin]+1)
+        return dp[amount] if dp[amount] <= amount else -1
