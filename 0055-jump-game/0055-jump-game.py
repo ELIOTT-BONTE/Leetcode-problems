dc59e6995dd471385b4965b530d85849f202a347
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-04-14 11:43:26 +0200

    Time: 37 ms (36.15%), Space: 13.4 MB (51.11%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,16 +1,14 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        reach = (len(nums)) *[0]
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        maxReach = 0
 
-        for i, n in enumerate(nums):
-            if n != 0:
-                for j in range(0, n):
-                    if (i+j) < len(reach)-1:
-                        reach[i+j] = 1
-                    elif (i+j) == len(reach)-1:
-                        return True
-            elif n == 0:
-                if i == len(reach)-1:
-                    return True
-                elif reach[i] == 0:
-                    return False
+        for index, num in enumerate(nums):
+            if maxReach < index:
+                return False
+            maxReach = max(maxReach, index + num)
+        return True
