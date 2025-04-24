76b02c3d983afe9c6e2c00b3a247d2f702b6a0ae
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-04-24 14:36:25 +0200

    Time: 0 ms (100%), Space: 18.8 MB (34.43%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,7 +2,7 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         # gauss formula
         n = len(nums)
-        expected_sum = n*(n+1)//2
+        expected_sum = n*(n+1)//2 # direct application of gauss
         actual_sum = 0
         for n in nums:
             actual_sum += n
