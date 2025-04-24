05f4e410d99708d1d28cdb82a3ca841b94a83a6b
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-04-24 14:21:58 +0200

    Time: 27 ms (16.74%), Space: 19 MB (18.13%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,15 +1,14 @@
+from collections import defaultdict
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        #compute expected sum
-        # 1 + 2 = 3
-        # 1 + 2 + 3 = 6
-        # 4 => 10
-        # 5 => 15
-        # general formula : n + (n+1) //2
-        n = len(nums)
-        expected = (n * (n+1))//2
-        #compute difference between expected sum and actuel sum
-        actual_sum = 0
-        for n in nums:
-            actual_sum += n
-        return expected - actual_sum
+        encountered = defaultdict(int)
+        for i, num in enumerate(nums):
+            encountered[i] -= 1 #-1 means should be encountered
+            encountered[num] += 1 #+1 means has been encountered
+        
+        
+        for n in encountered.keys():
+            if encountered[n] == -1:
+                return n
+        return len(nums)
