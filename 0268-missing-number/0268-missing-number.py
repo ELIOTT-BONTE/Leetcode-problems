9a45337682e888535e9678e63b886786ef1fe9dd
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-04-24 14:32:36 +0200

    Time: 1 ms (69.72%), Space: 18.5 MB (92.56%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,9 @@
-from collections import defaultdict
-
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        encountered = defaultdict(int)
+        #bit manipulation
+        #XOR each number with its index
+        #when the two are present in the equation, they cancel each other
+        missing = len(nums)
         for i, num in enumerate(nums):
-            encountered[i] -= 1 #-1 means should be encountered
-            encountered[num] += 1 #+1 means has been encountered
-        
-        
-        for n in encountered.keys():
-            if encountered[n] == -1:
-                return n
-        return len(nums)
+            missing ^= i ^ num
+        return missing
