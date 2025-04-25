374e49121e6f8ade635b9b05e70e942f43ff7ab9
ELIOTT-BONTE <eliottbontepro@gmail.com>
2025-04-25 13:44:23 +0200

    Time: 3 ms (65.87%), Space: 17.8 MB (74.36%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.py
+++ 0837-most-common-word/0837-most-common-word.py
@@ -3,12 +3,12 @@ import collections
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         # convert to lowercase
-        lower = paragraph.lower()
+        words = paragraph.lower()
         # separate in words
-        words = re.findall(r'\w+', lower)
+        words = re.findall(r'\w+', words)
         # filter out banned words
-        words_filtered = [word for word in words if word not in banned]
+        words = [word for word in words if word not in banned]
         # count word occurences
-        word_count = collections.Counter(words_filtered)
+        words = collections.Counter(words)
         # return max occurence word
-        return max(word_count, key = word_count.get)
+        return max(words, key = words.get)
